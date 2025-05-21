48139de6ca3f533961ed341be5c67aef43d00720
bernabarros <bernardobarros.work@gmail.com>
2025-05-21 13:17:46 +0100

    Create project HowManyOfThisType

---
+++ HowManyOfThisType/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HowManyOfThisType
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
