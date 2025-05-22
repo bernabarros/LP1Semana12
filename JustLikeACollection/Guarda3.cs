977330edc129427d647427abe26da4a481c79073
bernabarros <bernardobarros.work@gmail.com>
2025-05-23 00:49:06 +0100

    Add JustLikeACollection code

---
+++ JustLikeACollection/Guarda3.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustLikeACollection
+{
+    public class Guarda3<T> : IEnumerable<T>
+    {
+        private T v1, v2, v3 = default;
+        public int Count{ get; set; }
+        public T GetItem(int i)
+        {
+            T titem = default;
+            switch (i)
+            {
+                case 0:
+                    {
+                        titem = v1;
+                        break;
+                    }
+                case 1:
+                    {
+                        titem = v2;
+                        break;
+                    }
+                case 2:
+                    {
+                        titem = v3;
+                        break;
+                    }
+                default:
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+            }
+            return titem;
+        }
+        public void SetItem(int i, T item)
+        {
+            {
+                switch (i)
+                {
+                    case 0:
+                        {
+                            v1 = item;
+                            break;
+                        }
+                    case 1:
+                        {
+                            v2 = item;
+                            break;
+                        }
+                    case 2:
+                        {
+                            v3 = item;
+                            break;
+                        }
+                    default:
+                        {
+                            throw new IndexOutOfRangeException();
+                        }
+                }
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return v1;
+            yield return v2;
+            yield return v3;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public void Add(T item)
+        {
+            int list_items = this.Count;
+
+            if (list_items == 0)
+            {
+                v1 = item;
+            }
+            else if (list_items == 1)
+            {
+                v2 = item;
+            }
+            else
+            {
+                v3 = item;
+            }
+            Count++;
+        }
+    }
+}
--- JustLikeACollection/Program.cs
+++ JustLikeACollection/Program.cs
@@ -4,9 +4,44 @@ namespace JustLikeACollection
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello LP1!");
+            Guarda3<string> g3s = new Guarda3<string>()
+            {
+                "Olá",
+                "lalala",
+                "Adeus"
+            };
+
+            Guarda3<float> g3f = new Guarda3<float>()
+            {
+                2f,
+                54.6f,
+                1f
+            };
+
+            Console.WriteLine("Strings:");
+            foreach (string s in g3s)
+            {
+                Console.WriteLine($"    {s}");
+            }
+
+            Console.WriteLine("Floats:");
+            foreach (float f in g3f)
+            {
+                Console.WriteLine($"    {f}");
+            }
+
+            // Expected output:
+
+            // Strings:
+            //     Olá
+            //     lalala
+            //     Adeus
+            // Floats:
+            //     2
+            //     54.6
+            //     1
         }
     }
 }
